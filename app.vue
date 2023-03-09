f15b3854d74636b5d53f4fcc849c1c79515d30a4
Marco <suess_1998@web.de>
2023-03-09 16:16:27 +0100

    install(vuetify)

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>HELLO WORLD</h1>
+    <v-alert text="test"></v-alert>
+    <v-col>
+        <v-btn icon="mdi-heart" color="primary"></v-btn>
+      </v-col>
   </div>
 </template>
