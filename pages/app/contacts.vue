810e99f99cc04313a201422c80545c1fb3d1790e
Marco <suess_1998@web.de>
2023-04-04 14:51:41 +0200

    feature(contact) contact modal

--- pages/app/contacts.vue
+++ pages/app/contacts.vue
@@ -23,7 +23,10 @@
                     :elevation="isHovering ? 4 : 2"
                     :class="{ 'on-hover': isHovering, selected: isSelected }"
                     v-bind="props"
-                    @click="select"
+                    @click="
+                      selectUser(contact);
+                      select(true);
+                    "
                   >
                     <v-avatar color="red" size="40" class="avatar mr-4">
                       <span>{{
@@ -50,35 +53,144 @@
       <div class="title d-flex align-center">
         <h1 class="mr-4">Contacts</h1>
         <p><strong> Better with a Team</strong></p>
-        <v-btn append-icon="mdi-file-edit-outline" class="ml-auto btn-default">
+
+        <v-btn
+          v-if="selectedUserData"
+          append-icon="mdi-file-edit-outline"
+          class="ml-auto btn-default"
+          @click="openEditModal = true"
+        >
           Edit Contact
         </v-btn>
       </div>
-      <template v-if="true">
+      <template v-if="selectedUserData">
         <div class="contact-profile d-flex mt-12 align-center">
           <v-avatar color="red" size="64" class="avatar mr-4">
-            <span class="text-h5">{{ "MS" }}</span>
+            <span class="text-h5">{{
+              getUserFullNameAbbr(
+                selectedUserData.firstName,
+                selectedUserData.lastName
+              )
+            }}</span>
           </v-avatar>
 
-          <p class="text-h6 ml-2">Marco Suess</p>
+          <p class="text-h6 ml-2">
+            {{ selectedUserData.firstName }} {{ selectedUserData.lastName }}
+          </p>
         </div>
 
         <h3 class="mt-12">Contact-Information</h3>
         <div class="d-flex mt-4">
           <strong style="width: 64px" class="mr-2">Email:</strong>
-          <p class="mail">marcosuess@web.de</p>
+          <p class="mail">{{ selectedUserData.email }}</p>
         </div>
         <div class="d-flex mt-4">
           <strong style="width: 64px" class="mr-2">Phone:</strong>
-          <p>+032432412132</p>
+          <p>+{{ selectedUserData.phoneNumber }}</p>
         </div>
       </template>
     </div>
   </v-container>
+
+  <v-dialog persistent v-model="openEditModal" width="64%" class="d-flex pa-0">
+    <v-card class="v-card-modal flex-row pa-0">
+      <v-icon
+        @click="closeDialog"
+        class="close-icon"
+        icon="mdi-close"
+        size="large"
+      ></v-icon>
+
+      <div class="left-title">
+        <img src="../../assets/images/logo.png" />
+        <h2 class="text-h5 mt-4"><strong>Edit Contact </strong></h2>
+        <div class="costume-divider"></div>
+      </div>
+
+      <div
+        class="right-edit h-100 d-flex flex-row align-center pl-8  pt-12"
+      >
+        <v-avatar color="red" size="96" class="avatar mr-4">
+          <span class="text-h4">{{
+            getUserFullNameAbbr(
+              selectedUserData.firstName,
+              selectedUserData.lastName
+            )
+          }}</span>
+        </v-avatar>
+
+        <div class="contact-form ml-6 w-100">
+          <v-text-field
+            v-model="selectedUserData.firstName"
+            :rules="nameRules"
+            label="First Name*"
+            variant="outlined"
+            required
+            append-inner-icon="mdi-account-outline"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="selectedUserData.lastName"
+            :rules="nameRules"
+            label="Last Name*"
+            variant="outlined"
+            required
+            append-inner-icon="mdi-account-outline"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="selectedUserData.phoneNumber"
+            :rules="phoneRules"
+            label="Phone Number*"
+            variant="outlined"
+            required
+            append-inner-icon="mdi-phone-outline"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="selectedUserData.email"
+            :rules="emailRules"
+            label="E-mail*"
+            variant="outlined"
+            required
+            append-inner-icon="mdi-email-outline"
+          ></v-text-field>
+        </div>
+      </div>
+
+      <div class="action">
+        <v-btn
+          class="mr-8"
+          :append-icon="'mdi-backup-restore'"
+          color="error"
+          variant="plain"
+          @click="clearForm"
+        >
+          Reset
+        </v-btn>
+
+        <v-btn
+          @click="submitForm()"
+          class="btn-save-contact"
+          append-icon="mdi-check"
+        >
+          Save Contact
+        </v-btn>
+      </div>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup>
 import { useUserStore } from "@/stores/user";
+
+const selectedUserData = ref();
+
+const openEditModal = ref(false);
+
+const selectUser = (user) => {
+  selectedUserData.value = user;
+};
 
 const letters = [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
 </script>
@@ -137,6 +249,63 @@ const letters = [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
 
   .mail {
     color: #0ff;
+  }
+}
+
+//modal
+
+.left-title {
+  background-color: $dark-navy;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  padding: 32px;
+  width: 30%;
+
+  img {
+    object-fit: cover;
+    width: 64px;
+  }
+
+  h2 {
+    color: white;
+  }
+}
+
+.right-edit {
+  padding-bottom: 96px;
+  width: 56%;
+}
+
+.costume-divider {
+  border: 1.8px solid $secondary;
+  width: 25%;
+  margin-top: 8px;
+}
+
+.close-icon {
+  position: absolute;
+  right: 16px;
+  top: 16px;
+  cursor: pointer;
+}
+
+.btn-save-contact {
+  background-color: $dark-navy;
+  color: white;
+}
+
+.action {
+  position: absolute;
+  bottom: 16px;
+  right: 16px;
+  display: flex;
+}
+
+.contact-form {
+  .v-text-field {
+    margin-top: 16px;
   }
 }
 </style>
